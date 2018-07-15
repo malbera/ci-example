2756cbbed828c2c9ce38ca088b2c4d648c52c8e3
Maxim Ralovec <m.ralovec@gmail.com>
2018-07-15 17:19:15 +0200

    [implementation] Getmapping try

--- src/main/java/com/ciexample/web/Application.java
+++ src/main/java/com/ciexample/web/Application.java
@@ -3,6 +3,7 @@ package com.ciexample.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,9 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class Application {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String index() {
+    @GetMapping("/")
+    @ResponseBody String sayHello() {
         return "Hello world";
     }
 
